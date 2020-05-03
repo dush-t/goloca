8470d3488d9ff0e789c35f906f374388a144b637
Dushyant Yadav <dushyant9309@gmail.com>
2020-05-03 18:20:01 +0530

    Add a little comment explaining ws

--- main.go
+++ main.go
@@ -11,6 +11,9 @@ func main() {
 	config := InitializeApp()
 
 	http.Handle("/add_datapoint", api.AddDataPoint(config.Pool))
+
+	// WebSocket, to be used during peak hours to save the overhead of establishing
+	// a new connection everytime
 	http.HandleFunc("/ws", api.AddDataPointSocket(config.SocketUpgrader, config.Pool))
 
 	log.Println("HTTP server starting at :8080")
